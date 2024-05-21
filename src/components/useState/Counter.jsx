44ca606204099183d532213fd26d02c06251d411
argierincon <argierincon@gmail.com>
2024-05-21 12:10:21 -0500

    feature: add elems

--- src/components/useState/Counter.jsx
+++ src/components/useState/Counter.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+import "../../assets/index.css";
+
 export const Counter = () => {
-  return <h1 className="text-3xl font-bold underline">ESTE ES MI COUNTER</h1>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <section>
+      <div className="card-primary m-auto mt-20">
+        <h2 className="heading-primary">
+          <span className="heading-gradient">COUNTER</span>
+        </h2>
+
+        <p className="text-6xl text-gray-700">{counter}</p>
+
+        <section className="mt-6 flex gap-2">
+          <button className="btn-primary btn-blue"> +1 </button>
+          <button className="btn-default btn-white"> Reset </button>
+          <button className="btn-primary btn-blue"> -1 </button>
+        </section>
+      </div>
+    </section>
+  );
 };
