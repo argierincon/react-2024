49757c7dc1d0c815cad280c080f1b2889a36df4c
argierincon <argierincon@gmail.com>
2024-05-21 17:39:39 -0500

    feature: implement random bg images

---
+++ src/assets/bg-cats/images.js
@@ -0,0 +1,29 @@
+import img1 from "./bg-cat-1.jpeg";
+import img2 from "./bg-cat-2.jpeg";
+import img3 from "./bg-cat-3.jpeg";
+import img5 from "./bg-cat-5.jpeg";
+import img6 from "./bg-cat-6.jpeg";
+import img8 from "./bg-cat-8.jpeg";
+import img9 from "./bg-cat-9.jpeg";
+import img10 from "./bg-cat-10.jpeg";
+import img11 from "./bg-cat-11.jpeg";
+import img12 from "./bg-cat-12.jpeg";
+import img13 from "./bg-cat-13.jpeg";
+import img14 from "./bg-cat-14.jpeg";
+
+const images = [
+  img1,
+  img2,
+  img3,
+  img5,
+  img6,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+];
+
+export default images;
