bd6d45c596ea9112cafd7c9fe1d37bdd196056f0
argierincon <argierincon@gmail.com>
2024-05-21 12:28:00 -0500

    feature: implement counter logic

--- src/components/useState/Counter.jsx
+++ src/components/useState/Counter.jsx
@@ -4,6 +4,19 @@ import "../../assets/index.css";
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
 
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    if (counter <= 0) return;
+    setCounter(counter - 1);
+  };
+
+  const handleReset = () => {
+    setCounter(0);
+  };
+
   return (
     <section>
       <div className="card-primary m-auto mt-20">
@@ -14,9 +27,21 @@ export const Counter = () => {
         <p className="text-6xl text-gray-700">{counter}</p>
 
         <section className="mt-6 flex gap-2">
-          <button className="btn-primary btn-blue"> +1 </button>
-          <button className="btn-default btn-white"> Reset </button>
-          <button className="btn-primary btn-blue"> -1 </button>
+          <button className="btn-primary btn-blue" onClick={handleIncrement}>
+            +1
+          </button>
+          <button className="btn-default btn-white" onClick={handleReset}>
+            Reset
+          </button>
+          <button
+            className={`btn-primary btn-blue ${
+              counter <= 0 ? "cursor-not-allowed" : ""
+            }`}
+            disabled={counter <= 0}
+            onClick={handleDecrement}
+          >
+            -1
+          </button>
         </section>
       </div>
     </section>
