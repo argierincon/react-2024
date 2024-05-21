8e3d49bbe841695848794ae5c2ddc9e0474ff808
argierincon <argierincon@gmail.com>
2024-05-21 12:41:17 -0500

    feature: add Counter

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,5 @@ import { Counter, Todo } from "./components/useState/Imports.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Counter />
-    <Todo />
   </React.StrictMode>
 );
