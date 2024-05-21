d86ef9a9193de22b2268c91d0016a181eacafd9d
argierincon <argierincon@gmail.com>
2024-05-21 11:12:59 -0500

    feature: add initial components

---
+++ src/components/useState/Counter.jsx
@@ -0,0 +1,3 @@
+export const Counter = () => {
+  return <h1 className="text-3xl font-bold underline">ESTE ES MI COUNTER</h1>;
+};
---
+++ src/components/useState/Todo.jsx
@@ -0,0 +1,3 @@
+export const Todo = () => {
+  return <h1 className="text-3xl font-bold underline">ESTE ES EL TODO</h1>;
+};
