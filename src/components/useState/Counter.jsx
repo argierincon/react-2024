350030d6ea6f27b436ab17775c8cb1c75e44a172
argierincon <argierincon@gmail.com>
2024-05-30 09:50:52 -0500

    refactor: update html structure

--- src/components/useState/Counter.jsx
+++ src/components/useState/Counter.jsx
@@ -31,62 +31,58 @@ export const Counter = () => {
   };
 
   return (
-    <section>
-      <div className="card-primary m-auto mt-20">
-        <section className="counter-card">
-          <h2 className="heading-primary">
-            <span className="heading-gradient">COUNTER</span>
-          </h2>
+    <section className="card-primary row-span-2">
+      <section className="counter-card">
+        <h2 className="heading-primary">
+          <span className="heading-gradient">COUNTER</span>
+        </h2>
+        <div>
           <div>
-            <div>
-              <label htmlFor="count" className="input-label">
-                Current count
-              </label>
-              <input
-                id="count"
-                type="text"
-                className="input-default w-24"
-                placeholder="Enter amount to change counter"
-                value={count}
-                onChange={handleChangeCount}
-              />
-            </div>
-
-            <button
-              className="btn-white--small mt-4 block"
-              onClick={handleCountReset}
-              disabled={count === 1}
-            >
-              Reset
-            </button>
+            <label htmlFor="count" className="input-label">
+              Current count
+            </label>
+            <input
+              id="count"
+              type="text"
+              className="input-default w-24"
+              placeholder="Enter amount to change counter"
+              value={count}
+              onChange={handleChangeCount}
+            />
           </div>
 
-          <p className="text-6xl text-gray-700">{counter}</p>
-        </section>
-
-        <section className="mt-6 flex gap-2">
           <button
-            className={`btn btn-blue ${
-              counter <= 0 ? "cursor-not-allowed" : ""
-            }`}
-            disabled={counter <= 0}
-            onClick={handleDecrement}
-          >
-            -{count}
-          </button>
-          <button
-            className="btn btn-white"
-            onClick={handleReset}
-            disabled={counter <= 0}
+            className="btn-white--small mt-4 block"
+            onClick={handleCountReset}
+            disabled={count === 1}
           >
             Reset
           </button>
+        </div>
 
-          <button className="btn btn-blue" onClick={handleIncrement}>
-            +{count}
-          </button>
-        </section>
-      </div>
+        <p className="text-6xl text-gray-700">{counter}</p>
+      </section>
+
+      <section className="mt-6 flex gap-2">
+        <button
+          className={`btn btn-blue ${counter <= 0 ? "cursor-not-allowed" : ""}`}
+          disabled={counter <= 0}
+          onClick={handleDecrement}
+        >
+          -{count}
+        </button>
+        <button
+          className="btn btn-white"
+          onClick={handleReset}
+          disabled={counter <= 0}
+        >
+          Reset
+        </button>
+
+        <button className="btn btn-blue" onClick={handleIncrement}>
+          +{count}
+        </button>
+      </section>
     </section>
   );
 };
