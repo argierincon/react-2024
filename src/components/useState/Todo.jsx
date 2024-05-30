c43f2790342a449b6abc81bb4ca5d208bc7aa1c5
argierincon <argierincon@gmail.com>
2024-05-30 09:51:41 -0500

    feature: add some validations

--- src/components/useState/Todo.jsx
+++ src/components/useState/Todo.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { SearchBox } from "./SearchBox";
 import "../../assets/index.css";
 
 export const Todo = () => {
@@ -7,16 +8,26 @@ export const Todo = () => {
 
   const [inputVal, setInputVal] = useState("");
 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredTasks = taskList.filter((task) =>
+    task.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   const onClearInput = () => {
     setInputVal("");
   };
 
   const onAddItem = () => {
-    // Mantengo el estado inicial y agrego el valor del input
     if (inputVal !== "") {
-      setTaskList([...taskList, inputVal]);
+      setTaskList((prevTaskList) => [...prevTaskList, inputVal]);
       onClearInput();
     }
+  };
+
+  const onEnterAdd = (e) => {
+    if (e.key !== "Enter") return;
+    onAddItem();
   };
 
   const onClearList = () => {
@@ -28,68 +39,81 @@ export const Todo = () => {
     setTaskList(taskCleaned);
   };
 
+  const handleSearchChange = (searchValue) => {
+    setSearchTerm(searchValue);
+  };
+
   return (
-    <>
-      <section className="card-primary m-auto mt-20">
-        <div>
-          <h2 className="heading-primary mb-4">
-            <span className="heading-gradient">TO DO LIST!</span>
-          </h2>
+    <section className="card-primary row-span-7">
+      <div>
+        <h2 className="heading-primary mb-4">
+          <span className="heading-gradient">TO DO LIST!</span>
+        </h2>
 
-          <input
-            type="text"
-            className="input-default"
-            placeholder="Add task"
-            value={inputVal}
-            onChange={(e) => setInputVal(e.target.value)}
-          />
+        <input
+          type="text"
+          className="input-default"
+          placeholder="Add task"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+          onKeyUp={(e) => onEnterAdd(e)}
+        />
 
-          <section className="mt-6 flex gap-2">
-            <button
-              className="btn btn-white"
-              onClick={onClearInput}
-              disabled={!inputVal}
-            >
-              Clear input
-            </button>
-            <button
-              className="btn btn-blue"
-              onClick={onAddItem}
-              disabled={!inputVal}
-            >
-              Add
-            </button>
-          </section>
-        </div>
+        <section className="mt-3 flex gap-2">
+          <button
+            className="btn-white--small"
+            onClick={onClearInput}
+            disabled={!inputVal}
+          >
+            Clear input
+          </button>
+          <button
+            className="btn-blue--small"
+            onClick={onAddItem}
+            disabled={!inputVal}
+          >
+            Add
+          </button>
+        </section>
+
+        {taskList.length > 3 && (
+          <SearchBox onSearchChange={handleSearchChange} />
+        )}
+      </div>
+
+      <>
+        {taskList.length > 0 && filteredTasks.length === 0 ? (
+          <p className="mt-3 text-gray-500">No matches found :c</p>
+        ) : (
+          <div className="mt-6 pl-4">
+            <ul className="list-disc">
+              {filteredTasks.map((item, idx) => (
+                <li className="mt-2" key={`${idx}-${item}`}>
+                  <div className="grid grid-cols-[1fr_70px] items-center">
+                    <p>{item}</p>
+                    <button
+                      className="btn-white--small"
+                      onClick={() => onClearItem(idx)}
+                    >
+                      <span>x</span>
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {taskList.length === 0 && (
+          <p className="mt-3 text-gray-500">The task list is empty.</p>
+        )}
 
         {taskList.length > 0 && (
-          <>
-            <div className="mt-6 pl-4">
-              <ul className="list-disc">
-                {taskList.map((item, idx) => {
-                  return (
-                    <li className="mt-2" key={`${idx}-${item}`}>
-                      <div className="grid grid-cols-[1fr_70px] items-center">
-                        <p>{item}</p>
-                        <button
-                          className="btn-white--small"
-                          onClick={() => onClearItem(idx)}
-                        >
-                          <span>x</span>
-                        </button>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-
-            <button className="btn-white--small mt-4" onClick={onClearList}>
-              Clear list
-            </button>
-          </>
+          <button className="btn-white--small mt-4" onClick={onClearList}>
+            Clear list
+          </button>
         )}
-      </section>
-    </>
+      </>
+    </section>
   );
 };
